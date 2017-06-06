11b135b6d89994a5d42addffe7ae6cf234ea9217
broonyboy <nbrown3@sky.com>
2017-06-06 15:48:21 +0100

    Adding colours.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
    gridSize = 16;
    drawBoard(gridSize);
-   
+   randTrue=false;
 
-    $('#board div').on('mouseenter', function() {
+    /* $('#board div').on('mouseenter', function() {
         $(this).addClass('hovered');
     }); 
     
-  /*   $('#board div').on('mouseout', function() {
+    $('#board div').on('mouseout', function() {
         $(this).removeClass('hovered');
     });  */
 });
@@ -22,8 +22,8 @@ function clearBoard() {
 
 function drawBoard(gridSize) {
      $('#board').empty();
-    var horiSize = Math.floor(600/gridSize);
-    var vertSize =(600/gridSize);
+    var horiSize = Math.floor(960/gridSize);
+    var vertSize =(960/gridSize);
     var divHeight = gridSize * vertSize;
     var divWidth = gridSize * horiSize;
     $('#board').css('height', divHeight);
@@ -40,5 +40,45 @@ function drawBoard(gridSize) {
 function etch() {
     $('#board div').on('mouseenter', function() {
         $(this).addClass('hovered')
+        if (randTrue = true) {
+            changeColour();
+            $(this).css("background-color",colour);
+        }
     });
+}
+
+function changeColour() {
+    colour = randomColour();
+    randTrue = true;
+    return randTrue;
+}
+
+function randomColour() {
+    var hexColor = [];
+    hexColor[0] = "#";
+    
+    for (var i=1; i<7; i++) {
+        var x = Math.floor((Math.random()*16));
+        
+        if (x>-10 && x <=15) {
+            switch(x)
+            {
+                case 10: x="a"
+                break;
+                case 11: x="b"
+                break;
+                case 12: x="c"
+                break;
+                case 13: x="d"
+                break;
+                case 14: x="e"
+                break;
+                case 15: x="f"
+                break;
+            }
+        }
+        hexColor[i] = x;
+    }
+    var cString = hexColor.join("");
+    return cString
 }
