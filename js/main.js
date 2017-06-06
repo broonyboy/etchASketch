bce12770f7e4a8e624ae82cdb35598b45a8331be
broonyboy <nbrown3@sky.com>
2017-06-06 14:38:30 +0100

    updtaed to change board size

--- js/main.js
+++ js/main.js
@@ -1,10 +1,7 @@
 $(document).ready(function () {
-   for (var i=0; i<16 ; i++) {
-       for (var x=0; x<16 ; x++) {
-           var unit = $("<div class='unit'></div>");
-           unit.appendTo('#board');
-       }
-   }  
+   gridSize = 16;
+   drawBoard(gridSize);
+   
 
     $('#board div').on('mouseenter', function() {
         $(this).addClass('hovered');
@@ -14,3 +11,34 @@ $(document).ready(function () {
         $(this).removeClass('hovered');
     });  */
 });
+
+function clearBoard() {
+    var gridSize = prompt('I have cleared the grid, what size do you want your new one.');
+    $('.unit').removeClass('hovered');
+    $('#board').empty();
+    drawBoard(gridSize);
+    etch();
+}
+
+function drawBoard(gridSize) {
+     $('#board').empty();
+    var horiSize = Math.floor(600/gridSize);
+    var vertSize =(600/gridSize);
+    var divHeight = gridSize * vertSize;
+    var divWidth = gridSize * horiSize;
+    $('#board').css('height', divHeight);
+    $('#board').css('width', divWidth);
+    console.log(horiSize,vertSize);
+    for (var i=0; i<gridSize; i++) {
+        for (var x=0; x<gridSize; x++) {
+            var unit = $("<div class='unit' style = 'height: " + vertSize + "px; width: " + horiSize + "px;' ></div>");
+            unit.appendTo('#board');
+        }
+    }    
+}
+
+function etch() {
+    $('#board div').on('mouseenter', function() {
+        $(this).addClass('hovered')
+    });
+}
